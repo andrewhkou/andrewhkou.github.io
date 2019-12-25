513aad56fc9461a527f502ded97687cd61225bf4
Andrew Kou <andrewhkou@gmail.com>
2019-12-24 20:55:28 -0800

    new website wooo

--- src/PersonalWebsite.js
+++ src/PersonalWebsite.js
@@ -9,56 +9,32 @@ import Contact from './Contact'
 
 class PersonalWebsite extends Component {
 
-  // constructor(props){
-  //   super(props);
-  //   this.myRef = React.createRef();
-  //   this.About = React.createRef();
-  //   this.Experiences = React.createRef();
-  //   this.Projects = React.createRef();
-  //   this.Contact = React.createRef();
-  // }
-
   render() {
     return (
-      <div className="stuff-lmao" data-scroll-speed="0.2">
-      {/* <ScrollView decelerationRate={0.5}>
-      </ScrollView>  */}
-        {/* <header className="Website-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-        <section>
-          <Home/>
-        </section>
-        <section>
-          <About/>
-        </section>
-        <section>
-          <Experiences/>
-        </section>
-        <section>
-          <Projects/>
-        </section>
-        <section>
-          <Contact/>
-        </section>
-
-        
-        {/* <About/>
-        <Experiences/>
-        <Projects/>
-        <Contact/> */}
-      </div>
+        <div className = "content-container">
+          <div className = "content"> 
+            <div className = "greeting-header">
+              👋 Hey! I'm Andrew.
+            </div>
+            <div className = "greeting-text">
+              This website's gonna look awesome real soon, but in the meantime here's a bit about me. 
+            </div>
+            <ul className = "about-points">
+              <li>Second year CS student at <span class="school"><a class="about-links" href="https://www.youtube.com/watch?v=5srhwqzBQE8" target="_blank">UC Berkeley</a></span></li>
+              <li>Incoming SWE intern at <span class = "intern"><a class="about-links" href="https://www.aboutamazon.com/" target="_blank">Amazon</a></span></li>
+              <li>Developer at <span class="codebase"><a class="about-links" href="https://codebase.berkeley.edu/about/" target="_blank">CodeBase</a></span></li>
+              <li>Former design consultant at <span class="uma"><a class="about-links" href="https://www.umaucberkeley.com/" target="_blank">UMA</a></span></li>
+              <li>Amateur music producer</li>
+            </ul>
+            <div className = "links-container">
+              <div className = "links"> 
+                <a class="link-text" href="https://drive.google.com/file/d/1r6ypijhuY4Yzyr4w5m7FxwBMgWxkS3kN/view?usp=sharing" target="_blank">resume</a>
+                <a class="link-text" href="https://www.linkedin.com/in/andrew-kou-6071b8137/" target="_blank">linkedin</a>
+                <a class="link-text" href="https://github.com/andrewhkou" target="_blank">github</a>
+              </div> 
+            </div>
+          </div>
+        </div>
     );
   }
 }
