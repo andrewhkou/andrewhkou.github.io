2e096a76dac39bcb58f3050533a37aae0450832e
Andrew Kou <andrewkou@Andrews-MacBook-Pro.local>
2022-09-30 15:32:20 -0700

    Updates

---
+++ precache-manifest.c0e8b8cc6e1be8055a101273f5778031.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5a4234dad5ab52bdc98",
+    "url": "/static/css/main.9843c7ff.chunk.css"
+  },
+  {
+    "revision": "a5a4234dad5ab52bdc98",
+    "url": "/static/js/main.a5a4234d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "b3d24cf8e9b4bc200b1b",
+    "url": "/static/js/2.b3d24cf8.chunk.js"
+  },
+  {
+    "revision": "2cee57e960a985f33cbe1254c06df2e0",
+    "url": "/index.html"
+  }
+];
